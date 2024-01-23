c14221eff7f6666f62d5b8966ee8c42de4cb1b37
Brian Lawi Ochiel <lawioochiel@gmail.com>
2024-01-23 12:31:00 +0300

    completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i in range(len(dwarves)):
+        print(f"{i + 1}. {dwarves[i]}")
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteers):
+    return [planeteer.capitalize() + '!' for planeteer in planeteers]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if len(word)>4:
+            return True
+    return (False) 
+
+def find_the_cheese(cheese_types):
+    for cheese in cheese_types:
+        if cheese == "gouda" or cheese == "cheddar" or cheese == "camembert":
+            return cheese
